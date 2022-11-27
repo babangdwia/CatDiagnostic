d8b1e530c86fbbc9345d1783a9ba4cc0d352825c
babangdwia <77609144+babangdwia@users.noreply.github.com>
2022-11-28 00:04:48 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+# echo '' >> .streamlit/credentials.toml
+# echo '' >> .streamlit/credentials.toml
+echo '[general]' >> .streamlit/credentials.toml
+echo 'email = "int.halim@gmail.com"' >> .streamlit/credentials.toml
+
+echo '' >> .streamlit/config.toml
+echo '' >> .streamlit/config.toml
+echo '[server]' >> .streamlit/config.toml
+echo 'headless = true' >> .streamlit/config.toml
+echo 'enableCORS=false' >> .streamlit/config.toml
+echo "port= $PORT" >> .streamlit/config.toml
